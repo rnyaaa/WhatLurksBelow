d5f71ee7ae807c6258b2ebfada7c0c387d268fe7
KristinBjarkadottir <kristinb23@ru.is>
2024-12-13 14:11:21 +0000

    ending scene

--- Assets/Scripts/HouseController.cs
+++ Assets/Scripts/HouseController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HouseController : MonoBehaviour
 {
     public GameObject house;
+    public GameObject jellyfish;
     public ValveInteraction valve1;
     public ValveInteraction valve2;
 
@@ -12,12 +13,16 @@ public class HouseController : MonoBehaviour
     void Start()
     {
         house.SetActive(false);
+        jellyfish.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(valve1.finished && valve2.finished)
-            house.SetActive(true);   
+        {
+            house.SetActive(true);  
+            jellyfish.SetActive(true);
+        }
     }
 }
